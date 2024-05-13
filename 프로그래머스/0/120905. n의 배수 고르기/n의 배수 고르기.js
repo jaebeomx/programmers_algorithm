82e7be93d86d194f738f3e603b27f71c5bc64a6f
jae_beom <94835872+jaebeomx@users.noreply.github.com>
2024-05-13 11:13:31 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,9 +1,3 @@
 function solution(n, numlist) {
-    let arr= [];
-    for(let i=0; i<numlist.length; i++){
-        if(numlist[i]%n==0){
-            arr.push(numlist[i]);
-        }
-    }
-    return arr;
+    return numlist.filter((item)=>item%n===0);
 }
