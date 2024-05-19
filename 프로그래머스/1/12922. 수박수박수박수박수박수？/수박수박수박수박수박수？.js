1c79c781637375d6b7ff5235307f89c27c0a7254
jae_beom <94835872+jaebeomx@users.noreply.github.com>
2024-05-19 21:48:49 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let arr= [];
+    for(let i=1; i<=n; i++){
+        if(i%2==1){
+            arr.push("수");
+        }else{
+            arr.push("박");
+        }
+    }
+    return arr.join("");
+}
