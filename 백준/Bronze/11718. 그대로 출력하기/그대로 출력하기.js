f5b5df6fb894a08e21197dd1f04f487d5626930d
jae_beom <94835872+jaebeomx@users.noreply.github.com>
2024-08-16 00:19:09 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 96 ms, Memory: 9492 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+
+// const input = fs.readFileSync("./test.txt", "utf-8");
+const input = fs.readFileSync("/dev/stdin", "utf-8");
+
+input.split("\n").forEach((item) => console.log(item));
