4beec24610db1c2df3fe93786196a4a163f76c7f
jae_beom <94835872+jaebeomx@users.noreply.github.com>
2024-11-06 23:05:48 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 96 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.js
@@ -0,0 +1,4 @@
+const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim();
+
+const result = ( BigInt(input) * BigInt(input - 1) * BigInt(input - 2) ) / BigInt(6);
+console.log(`${result}\n${3}`);
